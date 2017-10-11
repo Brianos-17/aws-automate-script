5a3dc55c0f51111b7d17f0236c230ad03335fad0
NGrant <niall.grant1992@gmail.com>
2017-10-11 22:48:47 +0100

    Added create_bucket()

--- run_newwebserver.py
+++ run_newwebserver.py
@@ -5,6 +5,7 @@ import boto3
 
 # Declaring ec2 variable
 ec2 = boto3.resource('ec2')
+s3 = boto3.resource('s3')
 
 # To create an instance and add a tag to it after creation
 def create_instance():
@@ -14,6 +15,16 @@ def create_instance():
     tags = [{'Key': 'Name', 'Value': value}]
     # Used for TagSpecification field to name the instance
     tag_spec = [{'ResourceType': 'instance', 'Tags': tags}]
+    
+# Function to create a new bucket
+# TODO: create while loop incase bucket name is not unique
+def create_bucket(name):
+    try:
+        response = s3.create_bucket(Bucket=bucket_name, CreateBucketconfiguration={'LocationConstraint': 'eu-west-1'})
+        print(response)
+    except Exception as error:
+        print(error)
+    
     
     #creation of instance
     ec2.create_instances(
